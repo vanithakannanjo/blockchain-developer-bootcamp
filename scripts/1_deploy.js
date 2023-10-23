9d7db54caf3722738027832f16037b93aa14ae98
vanithakannanjo <vanithakannanjo@gmail.com>
2023-10-23 14:51:49 +0800

    hardhat config and console

---
+++ scripts/1_deploy.js
@@ -0,0 +1,27 @@
+async function main() {
+	//Fetch contract to deploy
+	const Token = await ethers.getContractFactory("Token")
+
+	//Deploy contract
+	const token = await Token.deploy()
+	await token.deployed()
+	console.log(`Token Deployed to : ${token.address}`)
+
+
+  
+}
+
+/*
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+*/
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
