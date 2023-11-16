f12f230782f83ac39dd74ccc77f5e56da5816332
Vanitha Kannan <vanithakannanjo@gmail.com>
2023-11-16 12:26:53 +0800

    Testing Contract

--- test/Token.js
+++ test/Token.js
@@ -1,18 +1,37 @@
-const { expect } = require('chai')
+const { expect } = require('chai');
+const { utils } = require('ethers');
+const { parseUnits } = require('ethers/lib/utils');
 const { ethers } = require('hardhat');
 
-describe('Token', () => {
-	//tests go inside here
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
-	it('has a name', async () => {
-		// Fetch token from block chain
+describe('Token Contract Test', () => {
+	let token
+	beforeEach(async () => {		
 		const Token = await ethers.getContractFactory('Token')
-		let token = await Token.deploy()
-
-		// Read token name
-		const name = await token.name()
-
-		// check that name is correct
-		expect(name).to.equal('VK token')
+		token = await Token.deploy('VK Token','VKT','1000000')
 	})
+
+	describe('Deployment', () => {
+		const name = 'VK Token'
+		const symbol = 'VKT'
+		const decimals = '18'
+		const totalSupply=tokens('1000000')
+		it('has correct name', async () => {
+			expect(await token.name()).to.equal(name)
+		})
+		it('has correct symbol', async () => {
+			expect(await token.symbol()).to.equal(symbol)
+		})
+		it('has correct decimals', async () => {
+			expect(Number(await token.decimals())).to.equal(Number(decimals))
+		})
+		it('has correct total supply', async () => {
+			expect(Number(await token.totalSupply())).to.equal(Number(totalSupply))
+		})
+    })
+
+	
 })
