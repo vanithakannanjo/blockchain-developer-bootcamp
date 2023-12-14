b8eb44d42fdaa82ca1c0328d6974493d11d7b705
Vanitha Kannan <vanithakannanjo@gmail.com>
2023-12-14 12:49:17 +0800

    markets listing

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar';
-
+import Markets from './Markets';
 function App() {
   const dispatch = useDispatch();
 
@@ -52,7 +52,7 @@ function App() {
 
       <main className="exchange grid">
         <section className="exchange__section--left grid">
-          {/* Markets */}
+          <Markets />
 
           {/* Balance */}
 
